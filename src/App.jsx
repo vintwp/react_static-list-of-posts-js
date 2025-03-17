f518be938dadae9040492cf1fecec67eebe8fec5
VitaliiPa <poroh1991ukr@gmail.com>
2025-03-17 20:07:43 +0200

    fixed key props

--- src/App.jsx
+++ src/App.jsx
@@ -23,6 +23,7 @@ export const App = () => {
   return (
     <section className="App">
       <h1 className="App__title">Static list of posts</h1>
+
       <PostList posts={posts} />
     </section>
   );
